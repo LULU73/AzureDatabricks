e625502a1fba807d342f03aa4712fd524ef1fd8f
LULU <30465242+LULU73@users.noreply.github.com>
2022-03-28 13:46:52 +0200

    Delete exo_print.py

--- exo_print.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-
-def print_hw():
-    dataframe = pd.read_csv('name_list.csv') 
-    for i in np.arange(len(dataframe)):
-        print('Hello world !', dataframe.iloc[i,:].values[0] )
-
